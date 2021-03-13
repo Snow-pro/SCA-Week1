6c20c407f06e6533b711925eeda26968047877d0
Obi-Nwosu Ife <ifechukwuobinwosu@gmail.com>
2021-03-14 00:27:21 +0100

    completed a for loop

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 #inchess
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He is {my_weight} pounds heavy.")
+print(f"Actually thats not too heavy.")
+print(f"He's got {my_eyes}.")
+
+#this line is tricky
+
+total = my_age + my_height + my_weight
+print(f"If i add {my_age}, {my_height}, and {my_weight} I get {total}.")
